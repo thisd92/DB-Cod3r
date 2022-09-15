3c65a45a712f0f3ea419f4a5f7d489ed49dd73d9
thisd92 <thiiagodutra@gmail.com>
2022-09-15 15:42:55 -0300

    create city table

---
+++ RelationalDB/createCityTable.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS cidades(
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(255) NOT NULL,
+    estado_id INT UNSIGNED NOT NULL,
+    area DECIMAL(10,2),
+    PRIMARY KEY (id),
+    FOREIGN KEY (estado_id) REFERENCES estados (id)
+);
