7d64565ae6d76a6c6d081d93b1cad254c3f09306
thisd92 <thiiagodutra@gmail.com>
2022-09-16 13:44:18 -0300

    insert mayors

---
+++ RelationalDB/insertMayor.sql
@@ -0,0 +1,12 @@
+SELECT * FROM `cidades`;
+
+INSERT INTO prefeitos
+    (nome, cidade_id)
+VALUES
+    ('Axel Schmidt Grael', 2),
+    ('Rodrigo Pinheiro', 4),
+    ('Glêdson Lima Bezerra', 6),
+    ('Dário Jorge Giolo Saadi', 1),
+    ('José Fortunati', null);
+
+SELECT * FROM `prefeitos`;
